c5b746e95359df1626ac8ddec05d04882ffaa38c
Olga <olga-klgd@yandex.ru>
2024-08-29 15:23:37 +0200

    Seventh homework

---
+++ src/HW7/main.js
@@ -0,0 +1,10 @@
+import makeList from "./makeList.js";
+
+export default function createContainer(id) {
+  const container = document.createElement("div");
+  container.id = id;
+  document.body.append(container);
+  return container;
+}
+
+makeList(createContainer("makeList"));
