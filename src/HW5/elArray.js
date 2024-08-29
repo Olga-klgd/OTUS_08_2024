9c250b1a605ddd48aef75482bfc3478f9acc27be
Olga <olga-klgd@yandex.ru>
2024-08-29 11:56:41 +0200

    Fifth homework

---
+++ src/HW5/elArray.js
@@ -0,0 +1,19 @@
+//Создавйте массив целых чисет из 10 элементов
+const arrayInput = [17, 2, 38, 4, 57, 6, 78, 8, 94, 10];
+//Создайте новый массив на основе исходного, в  котором каждый элемент будет вдвое больше элемента исходного массива с таким же индексом.
+// export
+function doubleArray() {
+  let clone = arrayInput.map((el) => el * 2);
+  console.log(clone);
+  return clone;
+}
+//Выведите в консоль сумму всех элементов массива.
+function sumArray() {
+  let summa = arrayInput.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0,
+  );
+  console.log(summa);
+  return summa;
+}
+export { doubleArray, sumArray };
